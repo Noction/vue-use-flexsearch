4c1f4599e62b43b496a12ef1ad663278e5690fb7
Soryn <soryngitlan@gmail.com>
2025-01-17 17:41:49 +0200

    ci: codecov action adjustments

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
-// vite.config.ts
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    outputFile: './test-report.junit.xml',
+    reporters: ['junit'],
     coverage: {
       reporter: ['cobertura'],
     },
